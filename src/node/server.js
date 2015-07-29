4e9596d69a0054d98cf0f563fabb0b7411af3b6e
Radu Paraschiv <xpilot@gmail.com>
2015-07-30 00:12:56 +0300

    Add app server and controlelrs

---
+++ src/node/server.js
@@ -0,0 +1,74 @@
+var http = require('http');
+var htmlPackage = {};
+var fs = require('fs');
+
+var ip =require('ip');
+
+var allowAccessControl = true;
+
+var handlers = {
+	slideHandler: {},
+	onRequest: function(request, response) {
+		//response.end('It Works!! Path Hit: ' + request.url);
+
+		var cmd = request.url.slice(1);
+
+		switch (cmd) {
+			case 'left': handlers.slideHandler('left'); break;
+			case 'right': handlers.slideHandler('right'); break;
+			case 'up': if(allowAccessControl) { handlers.slideHandler('up'); } break;
+			case 'down': if(allowAccessControl) { handlers.slideHandler('down'); } break;
+			case 'stop': allowAccessControl = false; break;
+		}
+
+		if(cmd === 'destroy') {
+			request.connection.unref(); 
+			localServer.close(); 
+			return void (0);
+		}
+
+		response.writeHead(200,{"Content-Type":"text/html"})
+		if(cmd === 'admiN') {
+			response.end(htmlPackage.admin);
+		}
+		else {
+			response.end(htmlPackage.client);
+		}
+	}
+};
+
+var localServer = null;
+
+module.exports = {
+	init: function (slideHandler) {
+		localServer = http.createServer(handlers.onRequest);
+		localServer.listen(1337);
+
+		// load client page
+		fs.readFile('./src/node/controller.html', 'utf8', function(err, data){
+			if(err) {
+				console.log(err);
+			}
+
+			htmlPackage.client = data.toString('utf8');
+		});
+
+		// load admin page
+		fs.readFile('./src/node/controllerAdmin.html', 'utf8', function(err, data){
+			if(err) {
+				console.log(err);
+			}
+
+			htmlPackage.admin = data.toString('utf8');
+		});
+
+
+		if(slideHandler) {
+			handlers.slideHandler = slideHandler;
+		}
+	},
+	
+	getIP: function() {
+		return ip.address();
+	}
+}
